d32ba589c6a6e4eb0ac0b414efb46282951db40c
ilovevar <ilovevar@gmail.com>
2025-12-04 12:54:54 +0000

    支持打开系统播放器

--- HassWebView.Core/HassJsBridge.cs
+++ HassWebView.Core/HassJsBridge.cs
@@ -1,8 +1,14 @@
 using System.Diagnostics;
 
 #if ANDROID
+using Android.Content;
 using Android.Webkit;
 using Java.Interop;
+using Microsoft.Maui.ApplicationModel;
+#endif
+
+#if WINDOWS
+using System;
 #endif
 
 namespace HassWebView.Core
@@ -22,6 +28,32 @@ namespace HassWebView.Core
         public void OpenVideoPlayer(string url)
         {
             Debug.WriteLine($"[{GetPlatform()}] OpenVideoPlayer with url: {url}");
+            if (string.IsNullOrEmpty(url))
+                return;
+
+#if ANDROID
+            try
+            {
+                var intent = new Intent(Intent.ActionView);
+                intent.SetDataAndType(global::Android.Net.Uri.Parse(url), "video/*");
+                intent.AddFlags(ActivityFlags.NewTask);
+                Platform.CurrentActivity?.StartActivity(intent);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error starting video player: {ex}");
+            }
+#elif WINDOWS
+            try
+            {
+                // Fire and forget
+                _ = global::Windows.System.Launcher.LaunchUriAsync(new Uri(url));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error starting video player: {ex}");
+            }
+#endif
         }
 
         private string GetPlatform()
