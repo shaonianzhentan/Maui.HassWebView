321a984e06128fd773c61fe670268c4375886ce8
ilovevar <ilovevar@gmail.com>
2025-12-05 06:50:05 +0000

    更新文本注入

--- HassWebView.Core/InputService.cs
+++ HassWebView.Core/InputService.cs
@@ -1,12 +1,19 @@
 namespace HassWebView.Core
 {
-    public static partial class InputService
+    public static class InputService
     {
         public static void InjectText(string text)
         {
-            PlatformInjectText(text);
+#if ANDROID
+            var inputMethodManager = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity?.GetSystemService(Android.Content.Context.InputMethodService) as Android.Views.InputMethods.InputMethodManager;
+            var view = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity?.CurrentFocus;
+            if (view != null && inputMethodManager != null)
+            {
+                inputMethodManager.ShowSoftInput(view, Android.Views.InputMethods.ShowFlags.Implicit);
+                var inputConnection = view.OnCreateInputConnection(new Android.Views.InputMethods.EditorInfo());
+                inputConnection?.CommitText(text, 1);
+            }
+#endif
         }
-
-        static partial void PlatformInjectText(string text);
     }
 }
